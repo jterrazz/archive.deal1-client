2da48d66a0b277c6e6a362404e8d81401dc65db3
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-03-24 23:29:15 +0100

    Save

--- app/web/babel.config.js
+++ app/web/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ["next/babel"],
     plugins: [
       [
         "module-resolver",
