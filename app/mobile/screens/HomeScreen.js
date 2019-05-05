82ca450b26ea202b98fdacfdf457d9709e7052c5
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-05-05 15:50:55 +0200

    Lint test

--- app/mobile/screens/HomeScreen.js
+++ app/mobile/screens/HomeScreen.js
@@ -15,21 +15,12 @@ export default class HomeScreen extends Component {
       <SafeAreaView style={defaultStyles.container}>
         <ScrollView style={defaultStyles.container}>
           <View style={{ padding: 14, paddingBottom: 0 }}>
-            <Text style={defaultStyles.textTitle}>
-              Welcome back Jean-Baptiste!
-            </Text>
-            <Text style={defaultStyles.textTitle}>
-              Connect now to your merchants
-            </Text>
+            <Text style={defaultStyles.textTitle}>Welcome back Jean-Baptiste!</Text>
+            <Text style={defaultStyles.textTitle}>Connect now to your merchants</Text>
             <Input
-              inputContainerStyle={[
-                defaultStyles.inputWhite,
-                { marginTop: sizes.base, marginBottom: sizes.base }
-              ]}
+              inputContainerStyle={[defaultStyles.inputWhite, { marginTop: sizes.base, marginBottom: sizes.base }]}
               leftIconContainerStyle={{ marginRight: sizes.base }}
-              leftIcon={
-                <Icon name="search" size={22} color={colors.textClearer} />
-              }
+              leftIcon={<Icon name="search" size={22} color={colors.textClearer} />}
             />
 
             <SegmentedSections sections={["Hot", "Next to you", "The best"]} />
